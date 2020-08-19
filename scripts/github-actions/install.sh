4c237436e7c2eeb18b418d5193a9e5fde0f7f7ce
Patrick Avery <patrick.avery@kitware.com>
2020-08-19 14:32:10 -0400

    Use previous version of swig on windows
    The newest version produces errors like the following:
    ```
    Run swig -python example.i
    (1) : Error: Unable to find 'swig.swg'
    (3) : Error: Unable to find 'python.swg'
    [error]Process completed with exit code 1.
    ```
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- scripts/github-actions/install.sh
+++ scripts/github-actions/install.sh
@@ -4,7 +4,15 @@ set -ev
 python -m pip install cibuildwheel==1.5.5
 
 if [[ $RUNNER_OS == "Windows" ]]; then
-  choco install -y swig
+  # Use a previous version of swig. The newest version (4.0.2)
+  # produces errors on windows like the following:
+  # ```
+  # Run swig -python example.i
+  # (1) : Error: Unable to find 'swig.swg'
+  # (3) : Error: Unable to find 'python.swg'
+  # ## [error]Process completed with exit code 1.
+  # ```
+  choco install -y swig --version=4.0.1
 elif [[ $RUNNER_OS == "macOS" ]]; then
   brew install swig
 elif [[ $RUNNER_OS == "Linux" ]]; then
