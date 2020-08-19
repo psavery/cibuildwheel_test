edb78bca94c94d0e33ca7a840036b83f9ccbdcd8
Patrick Avery <patrick.avery@kitware.com>
2020-08-19 14:02:08 -0400

    Try github actions
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

---
+++ scripts/github-actions/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ev
+
+python -m pip install cibuildwheel==1.5.5
+
+if [[ $RUNNER_OS == "Windows" ]]; then
+  choco install -y swig
+elif [[ $RUNNER_OS == "macOS" ]]; then
+  brew install swig
+elif [[ $RUNNER_OS == "Linux" ]]; then
+  apt-get install -y swig
+fi
